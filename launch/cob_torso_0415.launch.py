6d22f222b1e9c37e332f7c490051511f2d426fd5
ipa-ych <st179367@stud.uni-stuttgart.de>
2024-04-15 18:04:49 +0200

    add arm to cob_body

---
+++ launch/cob_torso_0415.launch.py
@@ -0,0 +1,140 @@
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, ExecuteProcess, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    # Initialize Arguments
+    robot = LaunchConfiguration("robot")
+    pkg_hardware_config = LaunchConfiguration("pkg_hardware_config")
+    description_file = LaunchConfiguration("description_file")
+
+    robotXacroName = 'cob4-25'
+
+    namePackage = 'cob_hardware_config'
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    # urdf = os.path.join(get_package_share_directory("cob_hardware_config"),"robots","cob4-25","urdf","cob4-25-copy.urdf")
+
+    urdf_file = os.path.join(get_package_share_directory("cob_sim"),"urdf","cob4-25_0415_torso_arm.urdf")
+    # Error using xacro file
+    world_file = os.path.join(get_package_share_directory("cob_sim"),"world","boxes.world")
+    # world_file = os.path.join(get_package_share_directory("cob_hardware_config"),"robots","cob4-25","urdf","ball.world")
+    # doc = xacro.parse(open(xacro_file))
+    # xacro.process_doc(doc)
+
+    # xacro parsing
+    doc = xacro.parse(open(urdf_file))
+    xacro.process_doc(doc)
+    #urdf parsing
+    params = {'robot_description': doc.toxml()}
+    robot_description = {'robot_description': doc.toxml()}
+
+    gazebo_rosPackageLaunch = PythonLaunchDescriptionSource(os.path.join(get_package_share_directory("gazebo_ros"),'launch', 'gazebo.launch.py'))
+
+    gazeboLaunch = IncludeLaunchDescription(gazebo_rosPackageLaunch, launch_arguments={'world': world_file}.items())
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    spawn_entity = Node(
+                package='gazebo_ros',
+                executable='spawn_entity.py',
+                output='screen',
+                arguments=['-topic','robot_description','-entity', robotXacroName])
+
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_tricycle_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'tricycle_controller'],
+        output='screen'
+    )
+
+    load_torso_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_trajectory_controller'],
+        output='screen'
+    )
+
+    twist_mux_params = os.path.join(get_package_share_directory('cob_sim'),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/tricycle_controller/cmd_vel')]
+        )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+    )
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+    )
+
+    cmd_vel_bridge_Twist = Node(
+        package="cob_sim",
+        executable="cmd_vel_bridge_Twist_node.py",
+        output='screen'
+    )
+
+    teleop_twist_joy_dir = get_package_share_directory('teleop_twist_joy')
+    joy_config = LaunchConfiguration('joy_config', default='xbox')
+
+    teleop_joy = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(teleop_twist_joy_dir, 'launch', 'teleop-launch.py')
+            ),
+            launch_arguments={'joy_config': joy_config}.items()
+        )
+
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_broadcaster],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_broadcaster,
+                on_exit=[load_tricycle_controller],
+            )
+        ),
+        gazeboLaunch,
+        rviz2,
+        node_robot_state_publisher,
+        spawn_entity,
+        twist_mux,
+        teleop_joy,
+        cmd_vel_bridge_Twist,
+        # joint_state_publisher_node,
+        robot_state_publisher_node,
+        load_torso_controller,
+        # rviz_node
+    ])
---
+++ launch/display_robot_0415.launch.py
@@ -0,0 +1,54 @@
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameters_type import ParameterValue
+import os
+import xacro
+import subprocess
+
+def generate_launch_description():
+
+    # xacro_file= os.path.join(get_package_share_directory("cob_hardware_config"),"robots","cob4-25","urdf","cob4-25.urdf-base.xacro")
+    xacro_file= os.path.join(get_package_share_directory("cob_sim"),"urdf","cob_right_arm_0415.urdf")
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    robot_description = {'robot_description': doc.toxml()}
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+    )
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log"
+    )
+
+    # gazebo_node = IncludeLaunchDescription(
+    #     launch_description_source=LaunchDescription([
+    #         ExecuteProcess(
+    #             cmd=["gazebo", "--verbose", "-s", "libgazebo_ros_factory.so"],
+    #             output="screen",
+    #             additional_env={'GAZEBO_MODEL_PATH': xacro_file},
+    #         )
+    #     ])
+    # )
+
+    nodes_to_start = [
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        rviz_node
+    ]
+
+    return LaunchDescription(nodes_to_start)
